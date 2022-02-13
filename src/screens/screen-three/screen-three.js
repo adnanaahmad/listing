1be2bde6ddf47152c582dd5f5296d012063b7bda
adnanahmad95 <adnan.ahmad@narsun.pk>
2022-02-13 20:03:03 +0500

    listing process screen made reuseable for screen 3

--- src/screens/screen-three/screen-three.js
+++ src/screens/screen-three/screen-three.js
@@ -6,6 +6,10 @@ import ListingProcessPage from '../../shared/pages/listing-process-page';
 import WelcomeImage from '../../assets/All Listings Assets/backpack.jpg'
 import SecondPage from './components/second-page';
 import FourthPage from './components/fourth-page';
+import LeftLeaf from '../../assets/Index Assets/Path 19@2x.png';
+import iconOne from '../../assets/All Listings Assets/Group 3@2x.png';
+import iconTwo from '../../assets/All Listings Assets/Group 60@2x.png';
+import iconThree from '../../assets/All Listings Assets/Group 4@2x.png';
 
 function ScreenThree() {
   const welcomePage = {
@@ -16,11 +20,24 @@ function ScreenThree() {
     showButtonGroup: false,
     menu: ['House Owners', 'Flatmates', 'Maintainance', 'Property Management'] 
   }
+  const listingProcess = {
+    title: 'Our Listing Process',
+    titleTheme: 'black',
+    cards: [
+        {title : 'MEET THE TEAM', icon: iconOne, text: 'Come and meet our team! Get to know your property manager before getting down to business'},
+        {title : 'VIEWING', icon: iconTwo, text: 'Book in a viewing for the properties which suit you best. We will advise you on the features of each home for your convenience and answer any questions you may have.'},
+        {title : 'APPLY NOW', icon: iconThree, text: `Apply for the home of your choosing quickly! You wouldn't want to miss out on the best opportunity.`}
+    ],
+    cardTheme: 'black',
+    boxShadow: 'rgb(203 146 108 / 35%) 0px 4px 12px',
+    leafImage: LeftLeaf,
+    leafOpacity: '.1'
+  }
   return (
     <React.Fragment>
       <WelcomePageVariantOne data = {welcomePage}/>
       <SecondPage/>
-      <ListingProcessPage/>
+      <ListingProcessPage data={listingProcess}/>
       <FourthPage/>
       <ClientsReviewPage/>
       <FooterPrimary/>
