b339575f9958f46e750733f9b9967cb81aa27e85
adnanahmad95 <adnan.ahmad@narsun.pk>
2022-02-13 19:30:10 +0500

    listing process screen made reuseable

--- src/screens/screen-fourteen/screen-fourteen.js
+++ src/screens/screen-fourteen/screen-fourteen.js
@@ -9,6 +9,11 @@ import imageOne from '../../assets/Homeowners Assets/DSC02202@2x.png';
 import imageTwo from '../../assets/Homeowners Assets/jack-levick-4qWq1poO-Qw-unsplash@2x.png';
 import CommitmentWorkPage from '../../shared/pages/commitment-work-page';
 import PropertiesTcoPage from '../../shared/pages/properties-tco-page';
+import LeftLeaf from '../../assets/Index Assets/Path 19@2x.png';
+import iconOne from '../../assets/Index Assets/Group 3@2x.png';
+import iconTwo from '../../assets/Index Assets/Group 5@2x.png';
+import iconThree from '../../assets/Index Assets/Group 4@2x.png';
+
 
 function ScreenFourteen() {
     const welcomePage = {
@@ -27,11 +32,25 @@ function ScreenFourteen() {
         theme: BrownTheme
     }
     const showHouse = false;
+
+    const listingProcess = {
+        title: 'Our Listing Process',
+        titleTheme: 'brown',
+        cards: [
+            {title : 'MEET THE TEAM', icon: iconOne, text: 'Come and meet our team! Get to know your property manager before getting down to business'},
+            {title : 'FREE APPRAISAL', icon: iconTwo, text: 'Call us for a free appraisal. We value your property as our own, reflected in the ongoing rental income and return on your investment.'},
+            {title : 'LIST PROPERTY', icon: iconThree, text: 'List your property on our site and we vow to find you the perfect tenants to suit your home. Leave it to us!'}
+        ],
+        cardTheme: 'green',
+        boxShadow: 'rgb(203 146 108 / 35%) 0px 4px 12px',
+        leafImage: LeftLeaf,
+        leafOpacity: '1'
+    }
     return (
         <React.Fragment>
         <WelcomePageVariantTwo data = {welcomePage}/>
         <CommitmentWorkPage data = {commitmentWorkPage}/>
-        <ListingProcessPage/>
+        <ListingProcessPage data={listingProcess}/>
         <PropertiesTcoPage data = {showHouse}/>
         <FooterSecondary/>
         </React.Fragment>
--- src/shared/pages/listing-process-page.js
+++ src/shared/pages/listing-process-page.js
@@ -2,48 +2,73 @@ import * as React from 'react';
 import { Stack, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import {toggleBorder} from '../styles/debugging-border';
-import iconOne from '../../assets/Index Assets/Group 3@2x.png';
-import iconTwo from '../../assets/Index Assets/Group 5@2x.png';
-import iconThree from '../../assets/Index Assets/Group 4@2x.png'
+import {BrownTheme} from '../styles/themes/brown-theme';
+import {DarkGreenTheme} from '../styles/themes/dark-green-theme';
 
-function ListingProcessPage() {
-    const isBorder = toggleBorder;
-    let Cards = [
-        {title : 'MEET THE TEAM', icon: iconOne, text: 'Come and meet our team! Get to know your property manager before getting down to business'},
-        {title : 'SHOWCASE', icon: iconTwo, text: 'We offer appraisals at no cost to you Showcase your home to your dedicated property manager and they will get you set up with the perfect investment.'},
-        {title : 'LIST PROPERTY', icon: iconThree, text: 'List your property on our site and we vow to find you the perfect tenants to suit your home. Leave it to us!'}
-    ];
+const isBorder = toggleBorder;
+
+function LeafImageBox(props) {  
+    return(
+        <Stack justifyContent="center" x={{border: isBorder ? '1px solid green' : 'none',}}>
+            <Box component="img"
+                sx={{
+                    height: '8rem',
+                    width: '8rem',
+                    objectFit: 'cover',
+                    transform: props.rightImage ? 'scaleX(-1)' : 'scaleX(1)',
+                    opacity: props.opacity
+                }}
+                alt="The house from the offer."
+                src={props.leafImage}
+            />
+        </Stack>
+    )
+}
+
+function ListingProcessPage(props) {
+    const themeObject = {
+        brown: BrownTheme.palette.primary.main,
+        black: 'black',
+        green: DarkGreenTheme.palette.primary.main
+
+    }
     return (
     <React.Fragment>
         <Stack direction="column" justifyContent="center" alignItems="center"sx={{ border: isBorder ? '1px solid red' : 'none', height: '78vh'}} spacing={'2rem'}>
-            <Typography variant="body1" sx={{fontWeight: 500, fontSize: '40px'}}>Our Listing Process</Typography>
-            <Stack direction="row" justifyContent="space-between" sx={{ border: isBorder ? '1px solid green' : 'none', maxWidth: '70%',  height: '48%'}} spacing={'2rem'}>
-            {
-                Cards.map((card, i) => (
-                    <Stack key={i} direction="column" justifyContent="center" alignItems="center" spacing={'1rem'} sx={{
-                        border: isBorder ? 1 : 'none',
-                        maxWidth: '30%',
-                        borderTopRightRadius: '40px',
-                        borderBottomLeftRadius: '40px',
-                        boxShadow: 'rgb(203 146 108 / 35%) 0px 4px 12px',
-                        paddingX: '.5rem'
-                    }}>
-                        <Box component="img"
-                            sx={{
-                                // height: '8rem',
-                                width: '9rem',
-                                objectFit: 'cover',
-                            }}
-                            alt="The house from the offer."
-                            src={card.icon}
-                        />
-                        <Typography variant="body1" sx={{fontWeight: 500, fontSize: '1.8rem', textAlign: 'center'}}>{card.title}</Typography>
-                        <Typography variant="body1" display="block" sx={{maxWidth: '550px', fontWeight: 400, fontSize: '1rem', textAlign: 'center'}}>
-                            {card.text}
-                        </Typography>
-                    </Stack>
-                )
-            )}
+            <Typography variant="body1" sx={{fontWeight: 500, fontSize: '40px', color: themeObject[props.data.titleTheme]}}>
+                {props.data.title}
+            </Typography>
+            <Stack direction="row" justifyContent="space-between" sx={{ border: isBorder ? '1px solid green' : 'none',  height: '48%', maxWidth: '1224px', width: '100%'}} spacing={'2rem'}>
+                <LeafImageBox opacity={props.data.leafOpacity} leafImage = {props.data.leafImage} rightImage={false}/>
+                {
+                    props.data.cards.map((card, i) => (
+                        <Stack key={i} direction="column" justifyContent="center" alignItems="center" spacing={'1rem'} sx={{
+                            border: isBorder ? 1 : 'none',
+                            maxWidth: '30%',
+                            borderTopRightRadius: '40px',
+                            borderBottomLeftRadius: '40px',
+                            boxShadow: props.data.boxShadow,
+                            paddingX: '.5rem',
+                        }}>
+                            <Box component="img"
+                                sx={{
+                                    // height: '8rem',
+                                    width: '9rem',
+                                    objectFit: 'cover',
+                                }}
+                                alt="The house from the offer."
+                                src={card.icon}
+                            />
+                            <Typography variant="body1" sx={{fontWeight: 500, fontSize: '1.8rem', textAlign: 'center', color: themeObject[props.data.cardTheme]}}>
+                                {card.title}
+                            </Typography>
+                            <Typography variant="body1" display="block" sx={{maxWidth: '550px', fontWeight: 400, fontSize: '1rem', textAlign: 'center', color: themeObject[props.data.cardTheme]}}>
+                                {card.text}
+                            </Typography>
+                        </Stack>
+                    )
+                )}
+                <LeafImageBox opacity={props.data.leafOpacity} leafImage = {props.data.leafImage} rightImage={true}/>
             </Stack>
         </Stack>        
     </React.Fragment>
