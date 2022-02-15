b1ac1f44e9cb81b50a5332edf9d5feff572f2944
adnanahmad95 <adnan.ahmad@narsun.pk>
2022-02-15 12:06:14 +0500

    s 18 in progress

--- src/screens/screen-eighteen/screen-eighteen.js
+++ src/screens/screen-eighteen/screen-eighteen.js
@@ -5,6 +5,11 @@ import { DarkGreenTheme } from '../../shared/styles/themes/dark-green-theme';
 import { BrownTheme } from '../../shared/styles/themes/brown-theme';
 import WelcomeImage from '../../assets/Property Management Assets/DSC00024@2x.png';
 import SecondSpacePage from '../../shared/pages/second-space-page';
+import ListingProcessPage from '../../shared/pages/listing-process-page';
+import LeftLeaf from '../../assets/Index Assets/Path 19@2x.png';
+import iconOne from '../../assets/All Listings Assets/Group 3@2x.png';
+import iconTwo from '../../assets/All Listings Assets/Group 60@2x.png';
+import iconThree from '../../assets/All Listings Assets/Group 4@2x.png';
 
 
 function ScreenEighteen() {
@@ -20,6 +25,19 @@ function ScreenEighteen() {
         menuTheme: DarkGreenTheme,
         headingTheme: BrownTheme,
     }
+    const listingProcess = {
+        title: 'Friendly Flat Process',
+        titleTheme: 'brown',
+        cards: [
+            {title : 'MEET THE TEAM', icon: iconOne, text: 'Come and meet our team! Get to know your property manager before getting down to business'},
+            {title : 'VIEWING', icon: iconTwo, text: 'Book in a viewing for the properties which suit you best. We will advise you on the features of each home for your convenience and answer any questions you may have.'},
+            {title : 'APPLY NOW', icon: iconThree, text: `Apply for the home of your choosing quickly! You wouldn't want to miss out on the best opportunity.`}
+        ],
+        cardTheme: 'green',
+        boxShadow: 'rgb(19 41 19 / 35%) 0px 4px 12px',
+        leafImage: LeftLeaf,
+        leafOpacity: '1'
+    }
     const spacePage = {
         theme: BrownTheme,
         showButton: false,
@@ -28,6 +46,7 @@ function ScreenEighteen() {
     return (
         <React.Fragment>
         <WelcomePageVariantTwo data = {welcomePage}/>
+        <ListingProcessPage data={listingProcess}/>
         <SecondSpacePage data={spacePage}/>
         <FooterSecondary/>
         </React.Fragment>
