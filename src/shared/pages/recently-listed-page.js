613e1ba29162080d82c6e4a65d357baa4b3bd8ee
adnanahmad95 <adnan.ahmad@narsun.pk>
2022-03-02 00:31:25 +0500

    screen one mobile responsive

--- src/shared/pages/recently-listed-page.js
+++ src/shared/pages/recently-listed-page.js
@@ -5,8 +5,25 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import homeImageOne from '../../assets/Homeowners Assets/Mask Group 4@2x.png'
 import homeImageTwo from '../../assets/All Listings Assets/francesca-tosolini-DmOhItSo49k-unsplash@2x.png'
 import ListCard from '../components/list-card/list-card';
+import { makeStyles, createStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    parentDiv: {
+        [theme.breakpoints.down('md')]: {
+            width: '100%'
+        },
+    },
+    moreDiv: {
+        [theme.breakpoints.down('md')]: {
+            marginRight: '0'
+        },
+    },
+  }),
+);
 
 function RecentlyListedPage(props) {
+    const classes = useStyles();
     const isBorder = toggleBorder;
     let Cards = [
         {price : '$450', address: '230 Queen Street', bed: 2, bath: 1, image: homeImageOne},
@@ -17,10 +34,10 @@ function RecentlyListedPage(props) {
     return (
         <React.Fragment>
             <Stack direction="column" justifyContent="center" alignItems="flex-end" sx={{ border: isBorder ? '1px solid red' : 'none', height: '78vh', backgroundColor: '#36400F'}} spacing={'2rem'}>
-                <Stack direction="column" justifyContent="space-between" sx={{border: isBorder ? 1 : 'none', height: '70%', width: '92%'}} spacing={'2rem'}>
+                <Stack className={classes.parentDiv} direction="column" justifyContent="space-between" sx={{border: isBorder ? 1 : 'none', height: '70%', width: '92%'}} spacing={'2rem'}>
                     <Stack direction="row" justifyContent="space-between">
                         <Typography variant="body1" sx={{fontWeight: 500, fontSize: '40px', color: 'white'}}>Recently Listed Properties</Typography>
-                        <Stack direction="row" alignItems="center" spacing={'2rem'} sx={{marginRight: '10%'}}>
+                        <Stack className={classes.moreDiv} direction="row" alignItems="center" spacing={'2rem'} sx={{marginRight: '10%'}}>
                             <Typography variant="body1" sx={{fontWeight: 500, fontSize: '20px', color: 'white'}}>Find More Listings</Typography>
                             <ArrowForwardIcon sx={{color: 'white'}}/>
                         </Stack>
