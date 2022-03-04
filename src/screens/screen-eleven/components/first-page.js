c65ef9ea1168741dc8d99a404734ae4b9ed72da4
adnanahmad95 <adnan.ahmad@narsun.pk>
2022-03-04 18:31:04 +0500

    screen 11 mobile responsive

--- src/screens/screen-eleven/components/first-page.js
+++ src/screens/screen-eleven/components/first-page.js
@@ -5,13 +5,17 @@ import {WhiteTheme} from '../../../shared/styles/themes/white-theme';
 import {DarkGreenTheme} from '../../../shared/styles/themes/dark-green-theme';
 import {ThemeProvider} from '@mui/material/styles';
 import Button from '@mui/material/Button';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 export default function FirstPage() {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up('md'));
     const isBorder = toggleBorder;
     return (
         <React.Fragment>
             <Stack direction="column" justifyContent="center" alignItems="center" sx={{border: isBorder ? '1px solid red' : 'none', borderBottom: '2px solid #0089ff99', height: 'fitContent', paddingY: '4rem'}}>
-                <Stack sx={{border: isBorder ? '2px solid yellow' : 'none', height: 'fitContent', width: '30%', maxWidth: '350px'}}>
+                <Stack sx={{border: isBorder ? '2px solid yellow' : 'none', height: 'fitContent', width: matches ? '30%' : '100%', maxWidth: '350px'}}>
                     <Stack spacing={'1.5rem'} alignItems="center" sx={{border: isBorder ? '2px solid orange' : 'none', borderRadius: '10px', height: 'fitContent', backgroundColor: WhiteTheme.palette.primary.contrastText, padding: '2rem 2rem'}} >
                         <Typography variant="h5" display="block" sx={{color: 'white'}}>Enquire</Typography>
                             <ThemeProvider theme={DarkGreenTheme}>
