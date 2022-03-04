380e1167d12ff3240942d8ab6151d765f5e9686f
adnanahmad95 <adnan.ahmad@narsun.pk>
2022-03-04 18:33:48 +0500

    screen 12 mobile responsive

--- src/screens/screen-twelve/components/first-page.js
+++ src/screens/screen-twelve/components/first-page.js
@@ -7,15 +7,18 @@ import {ThemeProvider} from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
 import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 export default function FirstPage() {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up('md'));
     const Buttons = ['Location', 'Rent per week', 'Bedrooms', 'Bathrooms', 'Property Type'];
-
     const isBorder = toggleBorder;
     return (
         <React.Fragment>
             <Stack direction="column" justifyContent="center" alignItems="center" sx={{border: isBorder ? '1px solid red' : 'none', borderBottom: '2px solid #0089ff99', height: 'fitContent', paddingY: '4rem'}}>
-                <Stack sx={{border: isBorder ? '2px solid yellow' : 'none', height: 'fitContent', width: '30%', maxWidth: '350px'}}>
+                <Stack sx={{border: isBorder ? '2px solid yellow' : 'none', height: 'fitContent', width: matches ? '30%' : '100%', maxWidth: '350px'}}>
                     <Stack spacing={'1.5rem'} alignItems="flex-start" sx={{border: isBorder ? '2px solid orange' : 'none', borderRadius: '10px', height: 'fitContent', backgroundColor: WhiteTheme.palette.primary.contrastText, padding: '1rem 1rem 2rem 1rem'}} >
                         <Stack spacing={'.5rem'} direction="row" justifyContent="flex-start" alignItems="center">
                             <MenuOutlinedIcon sx={{color: 'white'}}/>
--- src/screens/screen-twelve/screen-twelve.js
+++ src/screens/screen-twelve/screen-twelve.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import FirstPage from './components/first-page';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/styles';
 
+const theme = createTheme();
 function ScreenTwelve() {
   return (
     <React.Fragment>
-        <FirstPage/>
+        <ThemeProvider theme={theme}>
+          <FirstPage/>
+        </ThemeProvider>
     </React.Fragment>
   );
 }
