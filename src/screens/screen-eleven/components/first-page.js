b1cc57bb031e8d22350c395aa7f89e81fdc98c58
Abdul Waheed <developer@prontodigital.com>
2022-03-31 17:20:53 +0500

    form added for enquiry and api to post data done

--- src/screens/screen-eleven/components/first-page.js
+++ src/screens/screen-eleven/components/first-page.js
@@ -8,6 +8,9 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
+import { useFormik } from 'formik';
+import axios from "axios";
+import { baseURL, apiRoute } from '../../../shared/utils/constants';
 
 const styleObject = {
     "& ::-webkit-input-placeholder": {
@@ -30,32 +33,70 @@ export default function FirstPage() {
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.up('md'));
     const isBorder = toggleBorder;
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            phone: "",
+            message: "",
+            enquiry_type: "enquiry"
+        },
+        onSubmit: (form) => {
+            axios.post(baseURL + apiRoute.enquiry, {enquiry : form}).then(res => console.log(res.data)).catch(err => console.log(err));
+        },
+    });
     return (
         <React.Fragment>
-            <Stack direction="column" justifyContent="center" alignItems="center" sx={{border: isBorder ? '1px solid red' : 'none', borderBottom: '2px solid #0089ff99', height: 'fitContent', paddingY: '4rem'}}>
+            <Stack direction="column" justifyContent="center" alignItems="center" sx={{border: isBorder ? '1px solid red' : 'none', height: 'fitContent', paddingY: '4rem'}}>
                 <Stack sx={{border: isBorder ? '2px solid yellow' : 'none', height: 'fitContent', width: matches ? '30%' : '100%', maxWidth: '350px'}}>
                     <Stack spacing={'1.5rem'} alignItems="center" sx={{border: isBorder ? '2px solid orange' : 'none', borderRadius: '10px', height: 'fitContent', backgroundColor: WhiteTheme.palette.primary.contrastText, padding: '2rem 2rem'}} >
                         <Typography variant="h5" display="block" sx={{color: 'white'}}>Enquire</Typography>
                             <ThemeProvider theme={DarkGreenTheme}>
                                 <Stack sx={{border: isBorder ? 1 : 'none', backgroundColor: 'white', width: '100%', borderRadius: '10px', paddingY: '.5rem'}} spacing={'.5rem'}>
-                                    <TextField id={'name'} variant="outlined" placeholder={'Your Name'} sx={styleObject} />
+                                    <TextField variant="outlined" placeholder={'Your Name'} sx={styleObject} 
+                                    id="name"
+                                    name="name"
+                                    value={formik.values.name}
+                                    onChange={formik.handleChange}
+                                    error={formik.touched.name && Boolean(formik.errors.name)}
+                                    type="text"
+                                    />
                                     <Stack style={{border: `.5px solid ${WhiteTheme.palette.primary.contrastText}`, width: '100%'}}></Stack>
-                                    <TextField id={'email'} variant="outlined" placeholder={'Email'} sx={styleObject} />
+                                    <TextField variant="outlined" placeholder={'Email'} sx={styleObject} 
+                                    id="email"
+                                    name="email"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
+                                    error={formik.touched.email && Boolean(formik.errors.email)}
+                                    type="email"
+                                    />
                                     <Stack style={{border: `.5px solid ${WhiteTheme.palette.primary.contrastText}`, width: '100%'}}></Stack>
-                                    <TextField id={'number'} variant="outlined" placeholder={'Phone Number'} sx={styleObject} />
+                                    <TextField variant="outlined" placeholder={'Phone Number'} sx={styleObject} 
+                                    id="phone"
+                                    name="phone"
+                                    value={formik.values.phone}
+                                    onChange={formik.handleChange}
+                                    error={formik.touched.phone && Boolean(formik.errors.phone)}
+                                    type="number"
+                                    />
                                 </Stack>
                                 <Stack sx={{border: isBorder ? 1 : 'none', backgroundColor: 'white', width: '100%', borderRadius: '10px', padding: '.5rem 0', height: '100%', minHeight: '100px'}}>
                                     <TextField
-                                    id="outlined-multiline-flexible"
                                     multiline
                                     maxRows={4}
                                     placeholder='Your message...'
                                     sx={styleObject}
+                                    id="message"
+                                    name="message"
+                                    value={formik.values.message}
+                                    onChange={formik.handleChange}
+                                    error={formik.touched.message && Boolean(formik.errors.message)}
+                                    type="text"
                                     />
                                 </Stack>
                             </ThemeProvider>
                             <ThemeProvider theme={WhiteTheme}>
-                                <Button variant="contained" sx={{px: 2, py: .5, borderRadius: 2, fontWeight: 400, textTransform: 'none', whiteSpace: 'nowrap', fontSize: '1rem'}}>
+                                <Button variant="contained" onClick={formik.handleSubmit} sx={{px: 2, py: .5, borderRadius: 2, fontWeight: 400, textTransform: 'none', whiteSpace: 'nowrap', fontSize: '1rem'}}>
                                     Submit
                                 </Button>
                             </ThemeProvider>
