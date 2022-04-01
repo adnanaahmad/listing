7f5c1d56c17e574496934838c0f93b90c8030ca3
Abdul Waheed <developer@prontodigital.com>
2022-04-01 12:50:59 +0500

    listings api added in recently listed properties in home pg

--- src/shared/pages/recently-listed-page.js
+++ src/shared/pages/recently-listed-page.js
@@ -2,13 +2,15 @@ import * as React from 'react';
 import { Stack, Typography } from '@mui/material';
 import {toggleBorder} from '../styles/debugging-border';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import homeImageOne from '../../assets/Homeowners Assets/Mask Group 4@2x.png'
-import homeImageTwo from '../../assets/All Listings Assets/francesca-tosolini-DmOhItSo49k-unsplash@2x.png'
+//import homeImageOne from '../../assets/Homeowners Assets/Mask Group 4@2x.png'
+//import homeImageTwo from '../../assets/All Listings Assets/francesca-tosolini-DmOhItSo49k-unsplash@2x.png'
 import ListCard from '../components/list-card/list-card';
 import { makeStyles, createStyles } from '@mui/styles';
 import { useTheme } from '@mui/material/styles';
 import Carousel from 'react-material-ui-carousel';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import axios from "axios";
+import { baseURL, httpMethod, apiRoute } from '../utils/constants';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -35,16 +37,41 @@ const useStyles = makeStyles((theme) =>
 );
 
 function RecentlyListedPage(props) {
+    const useEffect = React.useEffect;
     const classes = useStyles();
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.up('md'));
     const isBorder = toggleBorder;
-    let Cards = [
-        {price : '$450', address: '230 Queen Street', bed: 2, bath: 1, image: homeImageOne},
-        {price : '$300', address: '120 Shortlant Street', bed: 5, bath: 2, image: homeImageTwo},
-        {price : '$500', address: '4 Alma Street', bed: 3, bath: 1, image: homeImageOne},
-        {price : '$230', address: '4 Ewan Street', bed: 2, bath: 1, image: homeImageTwo}
-    ];
+    // let Cards = [
+    //     {price : '$450', address: '230 Queen Street', bed: 2, bath: 1, image: homeImageOne},
+    //     {price : '$300', address: '120 Shortlant Street', bed: 5, bath: 2, image: homeImageTwo},
+    //     {price : '$500', address: '4 Alma Street', bed: 3, bath: 1, image: homeImageOne},
+    //     {price : '$230', address: '4 Ewan Street', bed: 2, bath: 1, image: homeImageTwo}
+    // ];
+    const [cards, setCards] = React.useState([]);
+    
+    useEffect(() => {
+        const getListings = axios.create({
+            baseURL: baseURL + apiRoute.getRooms,
+            method: httpMethod.get,
+        });
+        getListings().then( res => {
+            const rooms = res.data.rooms;
+            rooms.forEach((room, index) => {
+                if(index > 3) return;
+                setCards( C => [...C, {
+                    price: room.rent,
+                    address: room.house.address.house_number +' '+room.house.address.street,
+                    bed: room.house.number_of_kitchen,
+                    bath: room.house.number_of_washrooms,
+                    image: room.images && room.images.length > 0 ? room.images[0].url : '',
+                    id: room.id
+                }]);
+            })
+        }).catch(err => {
+            console.log(err);
+        });
+    }, []);
     return (
         <React.Fragment>
             <Stack direction="column" justifyContent="center" alignItems="flex-end" sx={{ border: isBorder ? '1px solid red' : 'none', height: '78vh', backgroundColor: '#36400F'}} spacing={'2rem'}>
@@ -60,7 +87,7 @@ function RecentlyListedPage(props) {
                         matches &&
                         <Stack direction="row" justifyContent="space-between" sx={{ border: isBorder ? '1px solid red' : 'none', maxWidth: '100%',  height: '80%'}} spacing={'2rem'}>
                         {
-                            Cards.map((card, i) => (
+                            cards.map((card, i) => (
                                 <ListCard boxShadow={false} key={i} theme={props.data.theme} data = {card} />
                             )
                         )}
@@ -75,7 +102,7 @@ function RecentlyListedPage(props) {
                             autoPlay = {false}
                             >
                                 {
-                                    Cards.map((card, i) => (
+                                    cards.map((card, i) => (
                                         <ListCard boxShadow={false} key={i} theme={props.data.theme} data = {card}
                                             styles={{imageHeight: '200px', cardMargin: 'auto', cardWidth: '250px'}}
                                         />
