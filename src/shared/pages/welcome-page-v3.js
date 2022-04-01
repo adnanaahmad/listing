5303c82e5983e4c1f5a8ae0d70b56408d66b4ff2
Abdul Waheed <developer@prontodigital.com>
2022-04-01 15:49:39 +0500

    cover photos on details page set according to room selected

--- src/shared/pages/welcome-page-v3.js
+++ src/shared/pages/welcome-page-v3.js
@@ -5,8 +5,8 @@ import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import {ThemeProvider } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import {toggleBorder} from '../styles/debugging-border';
-import sofaImage from '../../assets/Homeowners Assets/sofa.png';
-import doorImage from '../../assets/Homeowners Assets/Mask Group 4@2x.png';
+//import sofaImage from '../../assets/Homeowners Assets/sofa.png';
+//import doorImage from '../../assets/Homeowners Assets/Mask Group 4@2x.png';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 import MenuButton from '../components/menu-button/menu-button';
@@ -17,22 +17,25 @@ const isBorder = toggleBorder;
 
 function WelcomePageVariantThree(props) {
   const theme = useTheme();
+  const useEffect = React.useEffect;
   const matches = useMediaQuery(theme.breakpoints.up('md'));
   const matchesMobile = useMediaQuery(theme.breakpoints.down('sm'));
-  const coverPhotos = [
-    {
-      image: doorImage,
-      width: '42.5%'
-    },
-    {
-      image: sofaImage,
-      width: '42.5%'
-    },
-    {
-      image: doorImage,
-      width: '15%'
+  const [coverPhotos, setCoverPhotos] = React.useState([]);
+  useEffect(() => {
+    function buildCover () {
+      const images = props.data.listingDetail.images || [];
+      if (images.length === 1) {
+        return [{image: images[0].url, width: '100%'}];
+      } else if (images.length === 2) {
+        return [{image: images[0].url, width: '50%'}, {image: images[1].url, width: '50%'}];
+      } else if (images.length > 2) {
+        return [{image: images[0].url, width: '33.333%'}, {image: images[1].url, width: '33.333%'}, {image: images[2].url, width: '33.333%'}];
+      } else {
+        return [{image: '', width: '100%'}];
+      }
     }
-  ];
+    setCoverPhotos(() => [...buildCover()]);
+  }, [props.data.listingDetail])
   return (
     <React.Fragment>
       <Stack direction="column" justifyContent="space-between" sx={{ border: isBorder ? '1px solid red' : 'none', height: matches ? '100vh' : 'fit-content', width: '100%'}}>
@@ -64,7 +67,7 @@ function WelcomePageVariantThree(props) {
                 sx={{
                   height: matchesMobile ? '35vh' : matches ? '100%': '70vh',
                   border: isBorder ? '1px solid pink' : 'none',
-                  width: '33.333%',
+                  width: item.width,
                   objectFit: 'cover',
                 }}
                 alt="The house from the offer."
