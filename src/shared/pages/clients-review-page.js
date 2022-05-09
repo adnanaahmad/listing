399c45d7c781d1bc72d1af2c47fc7575789fb82f
Adnan Ahmad <adnanahmad.khayal@gmail.com>
2022-05-09 19:52:49 +0500

    caraousel added in client review page

--- src/shared/pages/clients-review-page.js
+++ src/shared/pages/clients-review-page.js
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import { Stack, Typography } from '@mui/material';
 import {toggleBorder} from '../styles/debugging-border';
-import {ThemeProvider} from '@mui/material/styles';
-import {GreenTheme} from '../styles/themes/green-theme'
 import Box from '@mui/material/Box';
-import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
-import CircleIcon from '@mui/icons-material/Circle';
 import clientImage from '../../assets/All Listings Assets/client.png';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
+import Carousel from 'react-material-ui-carousel';
 
 function ClientsReviewPage() {
     const isBorder = toggleBorder;
@@ -19,32 +16,41 @@ function ClientsReviewPage() {
         <Stack direction="column" justifyContent="center" alignItems="center" sx={{ border: isBorder ? '1px solid red' : 'none', minHeight: '100vh', height: 'fit-content'}}>
             <Stack direction="column" justifyContent="center" alignItems="center" sx={{ border: isBorder ? '1px solid maroon' : 'none', height: '75%', paddingX: '1rem'}} spacing={'2rem'}>
                 <Typography variant="h3" sx={{textAlign: 'center'}}>Hear From Our Clients</Typography>
-                <Stack direction={matches ? "row" : "column"} alignItems="center" justifyContent="space-between" spacing={'2rem'} sx={{ border: isBorder ? '1px solid orange' : 'none', width: '90%', height: '80%'}}>
-                    <Box component="img"
-                        sx={{
-                            maxHeight: matches ?'500px' : '300px',
-                            objectFit: 'cover',
-                            borderRadius: '40px'
-                        }}
-                        alt="The house from the offer."
-                        src={clientImage}
-                    />
-                    <Stack direction="column" justifyContent="space-between" alignItems="flex-start" sx={{ border: isBorder ? '1px solid green' : 'none', width: matches ?'40%' : '100%'}} spacing={'1rem'}>
-                        <Typography variant="h5" sx={{fontWeight: 500}}>Matthew VanSant</Typography>
-                        <Typography variant="body1" display="block" sx={{maxWidth: '600px'}}>
-                            Friendly flats are a pretty cool bunch of people who got me the right apartment for my price range, I guess I can writ anything here as its just a template so think refrigerators are pretty cool, capt they need to be big enough to fit containers on the shelves it containers cannot fit then its useless, buy a new fridge
-                        </Typography>
-                        <Stack direction="row" alignItems="center" justifyContent="center" spacing={'.5rem'} sx={{ border: isBorder ? 1 : 'none', width: '100%'}}>
-                            <ThemeProvider theme={GreenTheme}>
-                                <CircleOutlinedIcon color="primary" sx={{fontSize: 'small'}}/>
-                                <CircleIcon color="primary" sx={{fontSize: 'small'}}/>
-                                <CircleOutlinedIcon color="primary" sx={{fontSize: 'small'}}/>
-                                <CircleOutlinedIcon color="primary" sx={{fontSize: 'small'}}/>
-                                <CircleOutlinedIcon color="primary" sx={{fontSize: 'small'}}/>
-                            </ThemeProvider>
-                        </Stack>
-                    </Stack>
-                </Stack>
+                <Carousel sx={{height: 'fitContent', width: '100%', minHeight: '600px', minWidth: matches ? '800px' : '100%'}}
+                indicators={true}
+                navButtonsAlwaysVisible={false}
+                autoPlay = {false}
+                indicatorIconButtonProps={{
+                    style: {
+                        padding: '5px',
+                        // color: GreenTheme.palette.primary.main,
+                        marginTop: '400px'
+                    }
+                }}
+                >
+                        {
+                            [1,2,3].map((node, index) => (
+                                <Stack key={index} direction={matches ? "row" : "column"} alignItems="center" justifyContent="space-between" spacing={'2rem'} sx={{ border: isBorder ? '1px solid orange' : 'none', width: 'fit-content', height: 'fit-content'}}>
+                                    <Box component="img"
+                                        sx={{
+                                            maxHeight: matches ?'500px' : '300px',
+                                            objectFit: 'cover',
+                                            borderRadius: '40px'
+                                        }}
+                                        alt="The house from the offer."
+                                        src={clientImage}
+                                    />
+                                    <Stack direction="column" justifyContent="space-between" alignItems="flex-start" sx={{ border: isBorder ? '1px solid green' : 'none', width: matches ?'40%' : '100%'}} spacing={'1rem'}>
+                                        <Typography variant="h5" sx={{fontWeight: 500}}>Matthew VanSant</Typography>
+                                        <Typography variant="body1" display="block" sx={{maxWidth: '600px'}}>
+                                            Friendly flats are a pretty cool bunch of people who got me the right apartment for my price range, I guess I can writ anything here as its just a template so think refrigerators are pretty cool, capt they need to be big enough to fit containers on the shelves it containers cannot fit then its useless, buy a new fridge
+                                        </Typography>
+                                    </Stack>
+                                </Stack>
+                            ))
+                        }
+
+                </Carousel>
             </Stack>
         </Stack>        
     </React.Fragment>
